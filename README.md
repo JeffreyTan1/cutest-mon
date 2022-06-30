ed44bb30cbfc3ce554d53363ee4e168ff5ab5d9f
Jeffrey Tan <S3851781@student.rmit.edu.au>
2022-06-30 19:30:27 +1000

    add next image and github link

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Create T3 App
+# Cutest-Mon
 
 This is an app bootstrapped according to the [init.tips](https://init.tips) stack, also known as the T3-Stack.
+Who is the cutest Pokemon? Find out now at cutest-mon.jeffreytan.dev
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,14 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import { trpc } from "@/utils/trpc";
 import { inferQueryResponse } from "./api/trpc/[trpc]";
+import { useEffect, useState } from "react";
+import Image from "next/image";
 
 const Home: NextPage = () => {
-  const pokemonQuery = trpc.useQuery(["pokemon.getRandomPokemon"]);
-  const { data, isLoading } = pokemonQuery;
-
+  const pokemonQuery = trpc.useQuery(["pokemon.getRandomPokemon"], {
+    refetchOnWindowFocus: false,
+  });
+  const { data, isFetching } = pokemonQuery;
   const voteMutation = trpc.useMutation(["pokemon.cast-vote"]);
 
   const voteCutest = (selected_id: number) => {
@@ -29,14 +32,10 @@ const Home: NextPage = () => {
     pokemonQuery.refetch();
   };
 
-  if (isLoading || !data) {
+  if (isFetching || !data) {
     return (
-      <div className="h-screen w-screen flex flex-col justify-center items-center">
-        {isLoading ? (
-          <div className="text-center">Loading...</div>
-        ) : (
-          <div className="text-center">Error</div>
-        )}
+      <div className="h-screen w-screen flex flex-col justify-center items-center text-center text-xl">
+        {isFetching ? <div>Loading...</div> : <div>Error</div>}
       </div>
     );
   }
@@ -48,7 +47,7 @@ const Home: NextPage = () => {
         <meta name="description" content="Vote for the cutest Pokemon" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className="h-screen w-screen flex flex-col justify-center items-center">
+      <div className="h-screen w-screen flex flex-col justify-center items-center relative">
         <div className="text-3xl font-medium">Which is cuter?</div>
         <div className="p-4" />
         <div className="min-w-[30%] mx-4 py-6 px-8 flex justify-center items-center bg-slate-300 rounded-lg shadow-lg">
@@ -59,6 +58,16 @@ const Home: NextPage = () => {
           <PokemonListing pokemon={data.secondPokemon} vote={voteCutest} />
         </div>
         <div className="p-4" />
+
+        <div className="absolute bottom-5">
+          <a
+            href="https://github.com/JeffreyTan1/cutest-mon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Github
+          </a>
+        </div>
       </div>
     </>
   );
@@ -80,7 +89,12 @@ const PokemonListing: React.FC<{
     <div className="w-1/3 flex flex-col justify-center items-center text-center text-gray-900 font-medium capitalize">
       <h2 className="text-xl">{props.pokemon.name}</h2>
       <div className="p-2" />
-      <img src={props.pokemon.sprite ? props.pokemon.sprite : ""} width="200" />
+      <Image
+        src={props.pokemon.sprite ? props.pokemon.sprite : ""}
+        width={200}
+        height={200}
+        alt={props.pokemon.name}
+      />
       <div className="p-2" />
       <button
         className="bg-pink-500 hover:bg-pink-400 text-white text-sm font-bold py-1.5 px-2 rounded w-full"
